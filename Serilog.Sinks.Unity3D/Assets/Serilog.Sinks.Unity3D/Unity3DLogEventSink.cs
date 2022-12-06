7cbe3f2036389df5ee344399c719409df0305666
Kurai András <k.andras9642@gmail.com>
2022-12-06 20:25:08 +0100

    use using statement

--- Serilog.Sinks.Unity3D/Assets/Serilog.Sinks.Unity3D/Unity3DLogEventSink.cs
+++ Serilog.Sinks.Unity3D/Assets/Serilog.Sinks.Unity3D/Unity3DLogEventSink.cs
@@ -16,29 +16,28 @@ namespace Serilog.Sinks.Unity3D
 
         public void Emit(LogEvent logEvent)
         {
-            using (var buffer = new StringWriter())
+            using var buffer = new StringWriter();
+
+            _formatter.Format(logEvent, buffer);
+
+            switch (logEvent.Level)
             {
-                _formatter.Format(logEvent, buffer);
+                case LogEventLevel.Verbose:
+                case LogEventLevel.Debug:
+                case LogEventLevel.Information:
+                    Debug.Log(buffer.ToString().Trim());
+                    break;
 
-                switch (logEvent.Level)
-                {
-                    case LogEventLevel.Verbose:
-                    case LogEventLevel.Debug:
-                    case LogEventLevel.Information:
-                        Debug.Log(buffer.ToString().Trim());
-                        break;
+                case LogEventLevel.Warning:
+                    Debug.LogWarning(buffer.ToString().Trim());
+                    break;
 
-                    case LogEventLevel.Warning:
-                        Debug.LogWarning(buffer.ToString().Trim());
-                        break;
+                case LogEventLevel.Error:
+                case LogEventLevel.Fatal:
+                    Debug.LogError(buffer.ToString().Trim());
+                    break;
 
-                    case LogEventLevel.Error:
-                    case LogEventLevel.Fatal:
-                        Debug.LogError(buffer.ToString().Trim());
-                        break;
-
-                    default: throw new ArgumentOutOfRangeException(nameof(logEvent.Level), "Unknown log level");
-                }
+                default: throw new ArgumentOutOfRangeException(nameof(logEvent.Level), "Unknown log level");
             }
         }
     }
